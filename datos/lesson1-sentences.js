221fe595bda493821ba8c0da9202af73b4d9217d
bensmithperez <bsmith@scnctech.com>
2023-01-05 20:47:59 -0800

    agrego contenido de leccion 1

---
+++ datos/lesson1-sentences.js
@@ -0,0 +1,30 @@
+let indiceMax = 28;
+let listaCompletaDeDatos = '[' +
+    '{"id":1,"word":"你好","pinyin":"nǐ hǎo","meaning":"hello"},' +
+    '{"id":2,"word":"早上好","pinyin":"zǎo shàng hǎo","meaning":"good morning"},' +
+    '{"id":3,"word":"晚上好","pinyin":"wǎn shàng hǎo","meaning":"good evening"},' +
+    '{"id":4,"word":"再见","pinyin":"zài jiàn","meaning":"see you again / good bye"},' +
+    '{"id":5,"word":"你叫什么名字","pinyin":"nǐ jiào shén me míng zì","meaning":"what is your name?"},' +
+    '{"id":6,"word":"我叫…","pinyin":"wǒ jiào …","meaning":"my name is…"},' +
+    '{"id":7,"word":"你多大了","pinyin":"nǐ duō dà liǎo","meaning":"how old are you?"},' +
+    '{"id":8,"word":"我…岁","pinyin":"wǒ … suì","meaning":"im … years old"},' +
+    '{"id":9,"word":"你是哪国人","pinyin":"nǐ shì nǎ guó rén","meaning":"what country are you from?"},' +
+    '{"id":10,"word":"你是哪里人","pinyin":"nǐ shì nǎ lǐ rén","meaning":"which place are you from?"},' +
+    '{"id":11,"word":"我是中国人","pinyin":"wǒ shì zhōng guó rén","meaning":"i\'m from china / i\'m chinese"},' +
+    '{"id":12,"word":"我不是美国人","pinyin":"wǒ bù shì měi guó rén","meaning":"i\'m not from china"},' +
+    '{"id":13,"word":"美国","pinyin":"měi guó","meaning":"USA"},' +
+    '{"id":14,"word":"英国","pinyin":"yīng guó","meaning":"the UK"},' +
+    '{"id":15,"word":"法国","pinyin":"fǎ guó","meaning":"France"},' +
+    '{"id":16,"word":"德国","pinyin":"dé guó","meaning":"Germany"},' +
+    '{"id":17,"word":"西班牙","pinyin":"xī bān yá","meaning":"Spain"},' +
+    '{"id":18,"word":"日本","pinyin":"rì běn","meaning":"Japan"},' +
+    '{"id":19,"word":"泰国","pinyin":"tài guó","meaning":"Thailand"},' +
+    '{"id":20,"word":"危地马拉","pinyin":"wēi dì mǎ lā","meaning":"Guatemala"},' +
+    '{"id":21,"word":"很高兴认识你！","pinyin":"hěn gāo xīng rèn shí nǐ","meaning":"nice to meet you"},' +
+    '{"id":22,"word":"我也是！","pinyin":"wǒ yě shì","meaning":"me too!"},' +
+    '{"id":23,"word":"你认识他吗","pinyin":"nǐ rèn shí tā má","meaning":"do you know him?"},' +
+    '{"id":24,"word":"我认识他","pinyin":"wǒ rèn shí tā","meaning":"i know him"},' +
+    '{"id":25,"word":"我不认识他","pinyin":"wǒ bù rèn shí tā","meaning":"i don\'t know him"},' +
+    '{"id":26,"word":"你喜欢他吗","pinyin":"nǐ xǐ huān tā má","meaning":"do you like him?"},' +
+    '{"id":27,"word":"我喜欢他","pinyin":"wǒ xǐ huān tā","meaning":"i like him"},' +
+    '{"id":28,"word":"我不喜欢他","pinyin":"wǒ bù xǐ huān tā","meaning":"i don\'t like him"}]';
---
+++ datos/lesson1-vocab.js
@@ -0,0 +1,28 @@
+let indiceMax = 26;
+let listaCompletaDeDatos = '[' +
+    '{"id":1,"word":"高兴","pinyin":"gāoxìng","meaning":"happy"},' +
+    '{"id":2,"word":"国","pinyin":"guó","meaning":"country; state; nation"},' +
+    '{"id":3,"word":"很","pinyin":"hěn","meaning":"very"},' +
+    '{"id":4,"word":"叫（动）","pinyin":"jiào","meaning":"call; be called"},' +
+    '{"id":5,"word":"名字","pinyin":"míngzi","meaning":"Name"},' +
+    '{"id":6,"word":"哪","pinyin":"nǎ","meaning":"which"},' +
+    '{"id":7,"word":"哪里","pinyin":"nǎ lǐ","meaning":"where"},' +
+    '{"id":8,"word":"你","pinyin":"nǐ","meaning":"you"},' +
+    '{"id":9,"word":"人","pinyin":"rén","meaning":"people"},' +
+    '{"id":10,"word":"认识","pinyin":"rènshi","meaning":"know"},' +
+    '{"id":11,"word":"什么","pinyin":"shénme","meaning":"What"},' +
+    '{"id":12,"word":"岁","pinyin":"suì","meaning":"year, age"},' +
+    '{"id":13,"word":"他","pinyin":"tā","meaning":"he"},' +
+    '{"id":14,"word":"她","pinyin":"tā","meaning":"she"},' +
+    '{"id":15,"word":"晚上","pinyin":"wǎnshang","meaning":"Night"},' +
+    '{"id":16,"word":"我","pinyin":"wǒ","meaning":"I"},' +
+    '{"id":17,"word":"也","pinyin":"yě","meaning":"also"},' +
+    '{"id":18,"word":"好（形）","pinyin":"hǎo","meaning":"good"},' +
+    '{"id":19,"word":"再见","pinyin":"zàijiàn","meaning":"Bye"},' +
+    '{"id":20,"word":"多（形、代）","pinyin":"duō","meaning":"many; much; more"},' +
+    '{"id":21,"word":"大","pinyin":"dà","meaning":"large, big"},' +
+    '{"id":22,"word":"了","pinyin":"le","meaning":"past tense marker"},' +
+    '{"id":23,"word":"是","pinyin":"shì","meaning":"yes"},' +
+    '{"id":24,"word":"喜欢","pinyin":"xǐhuan","meaning":"like"},' +
+    '{"id":25,"word":"不","pinyin":"bù","meaning":"No"},' +
+    '{"id":26,"word":"早上","pinyin":"zǎoshang","meaning":"Morning"}]';
