b759aea75f54d565940ba40d6d4e7014cff02194
bensmithperez <bsmith@scnctech.com>
2023-01-07 19:53:14 -0800

    agregamos cosas a lesson 2

--- datos/lesson2-vocab.js
+++ datos/lesson2-vocab.js
@@ -1,4 +1,4 @@
-let indiceMax = 13;
+let indiceMax = 28;
 let listaCompletaDeDatos = '[' +
     '{"id":1,"word":"这","pinyin":"zhè","meaning":"this"},' +
     '{"id":2,"word":"那","pinyin":"nà","meaning":"that"},' +
@@ -12,19 +12,20 @@ let listaCompletaDeDatos = '[' +
     '{"id":10,"word":"苹果","pinyin":"píngguǒ","meaning":"apple"},' +
     '{"id":11,"word":"苹果手机","pinyin":"píngguǒ shǒujī","meaning":"iPhone (apple phone)"},' +
     '{"id":12,"word":"苹果电脑","pinyin":"píngguǒ diànnǎo","meaning":"macbook (apple computer)"},' +
-    '{"id":13,"word":"鸡蛋","pinyin":"jīdàn","meaning":"egg (chicken egg)"}'+
-    '{"id":14,"word":"中文","pinyin":"zhōngwén","meaning":"chinese language"}'+
-    '{"id":15,"word":"汉语","pinyin":"hànyǔ","meaning":"chinese language"}'+
-    '{"id":16,"word":"英文","pinyin":"yīngwén","meaning":"english language"}'+
-    '{"id":17,"word":"英语","pinyin":"yīngyǔ","meaning":"english language"}'+
-    '{"id":18,"word":"日文","pinyin":"rìwén","meaning":"japanese language"}'+
-    '{"id":19,"word":"日语","pinyin":"rìyǔ","meaning":"japanese languaje"}'+
-    '{"id":20,"word":"发文","pinyin":"fāwén","meaning":"french language"}'+
-    '{"id":21,"word":"发育","pinyin":"fāyù","meaning":"french language"}'+
-    '{"id":22,"word":"西班牙文","pinyin":"xībānyáwén","meaning":"spanish language"}'+
-    '{"id":23,"word":"西班牙语","pinyin":"xībānyáyǔ","meaning":"spanish language"}'+
-    '{"id":24,"word":"泰文","pinyin":"tàiwén","meaning":"thai language"}'+
-    '{"id":25,"word":"泰语","pinyin":"tàiyǔ","meaning":"thai language"}'+
-    '{"id":26,"word":"德文","pinyin":"déwén","meaning":"german language"}'+
-    '{"id":27,"word":"德语","pinyin":"déyǔ","meaning":"german language"}'+
+    '{"id":13,"word":"鸡蛋","pinyin":"jīdàn","meaning":"egg (chicken egg)"},'+
+    '{"id":14,"word":"中文","pinyin":"zhōngwén","meaning":"chinese language"},'+
+    '{"id":15,"word":"汉语","pinyin":"hànyǔ","meaning":"chinese language"},'+
+    '{"id":16,"word":"英文","pinyin":"yīngwén","meaning":"english language"},'+
+    '{"id":17,"word":"英语","pinyin":"yīngyǔ","meaning":"english language"},'+
+    '{"id":18,"word":"日文","pinyin":"rìwén","meaning":"japanese language"},'+
+    '{"id":19,"word":"日语","pinyin":"rìyǔ","meaning":"japanese languaje"},'+
+    '{"id":20,"word":"发文","pinyin":"fāwén","meaning":"french language"},'+
+    '{"id":21,"word":"发育","pinyin":"fāyù","meaning":"french language"},'+
+    '{"id":22,"word":"西班牙文","pinyin":"xībānyáwén","meaning":"spanish language"},'+
+    '{"id":23,"word":"西班牙语","pinyin":"xībānyáyǔ","meaning":"spanish language"},'+
+    '{"id":24,"word":"泰文","pinyin":"tàiwén","meaning":"thai language"},'+
+    '{"id":25,"word":"泰语","pinyin":"tàiyǔ","meaning":"thai language"},'+
+    '{"id":26,"word":"德文","pinyin":"déwén","meaning":"german language"},'+
+    '{"id":27,"word":"德语","pinyin":"déyǔ","meaning":"german language"},'+
+    '{"id":28,"word":"老师","pinyin":"","meaning":"teacher"}'+
     ']';
