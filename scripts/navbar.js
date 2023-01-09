2e4fe0cdc63ece17644f99116f67a9131f6602c1
bensmithperez <bsmith@scnctech.com>
2023-01-09 08:45:21 -0800

    vamos de a poco agregando cosas de js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,20 +1,22 @@
-function cargarListaDeLinks(urlDeEstaPagina) {
-    const tiposDeLink = {
-        main: 0,
-        sub: 1,
-        header: 2,
-    }
-    let nombres = ["Home", "Numbers", "Lesson 1", "Vocab", "Sentences", "Lesson 2", "Vocab", "Sentences"];
+const tiposDeLink = {
+    main: 0,
+    sub: 1,
+    header: 2,
+}
 
-    let urls = [
-        { "index.html": tiposDeLink.main }, 
-        { "numeros.html": tiposDeLink.main }, 
-        { "lesson1": tiposDeLink.header }, 
-        { "lesson1-vocab.html": tiposDeLink.sub }, 
-        { "lesson1-sentences.html": tiposDeLink.sub },
-        { "lesson2": tiposDeLink.header }, 
-        { "lesson2-vocab.html": tiposDeLink.sub }, 
-        { "lesson2-sentences.html": tiposDeLink.sub }];
+let nombres = ["Home", "Numbers", "Lesson 1", "Vocab", "Sentences", "Lesson 2", "Vocab", "Sentences"];
+
+let urls = [
+    { "index.html": tiposDeLink.main },
+    { "numeros.html": tiposDeLink.main },
+    { "lesson1.html": tiposDeLink.header },
+    { "lesson1-vocab.html": tiposDeLink.sub },
+    { "lesson1-sentences.html": tiposDeLink.sub },
+    { "lesson2.html": tiposDeLink.header },
+    { "lesson2-vocab.html": tiposDeLink.sub },
+    { "lesson2-sentences.html": tiposDeLink.sub }];
+
+function cargarListaDeLinks(urlDeEstaPagina) {
     let menuCompleto = ``;
     index = 0;
     for (const url of urls) {
@@ -22,11 +24,11 @@ function cargarListaDeLinks(urlDeEstaPagina) {
             urlPorUsar = "";
             idParaElBanco = "pagina-" + urlPagina.replace(".html", "");
             // pongo sin urls los que son headers o la url actual
-            if (urlPagina == urlDeEstaPagina || tipoLink == tiposDeLink.header) {
-                urlPorUsar = "#";
-            } else {
-                urlPorUsar = urlPagina;
-            }
+            // if (urlPagina == urlDeEstaPagina || tipoLink == tiposDeLink.header) {
+            // urlPorUsar = "#";
+            // } else {
+            urlPorUsar = urlPagina;
+            // }
 
             menuCompleto += `<a id="` + idParaElBanco + `" href="` + urlPorUsar + `" class="menu-principal-link`;
 
@@ -46,13 +48,14 @@ function cargarListaDeLinks(urlDeEstaPagina) {
             }
 
             // agrego clase para mostrar como seleccionado
-            if (urlPagina == urlDeEstaPagina ) {
+            if (urlPagina == urlDeEstaPagina) {
                 menuCompleto += ` item-menu-seleccionado`;
             }
 
             menuCompleto += `">` + nombres[index] + `</a>`;
 
             index += 1;
+            console.log(urlDeEstaPagina, tipoLink, idParaElBanco)
         }
     }
     document.getElementById("menu-principal").innerHTML = menuCompleto;
@@ -60,4 +63,20 @@ function cargarListaDeLinks(urlDeEstaPagina) {
 
 function marcarHeader(id) {
     document.getElementById(id).classList.add("header-seleccionado");
+}
+
+function crearMenuDeSeccion(id) {
+    menuSeccion = ``;
+    index = 0;
+    for (const url of urls) {
+        for (const [urlPagina, tipoLink] of Object.entries(url)) {
+            console.log(urlPagina)
+            if (urlPagina.includes(id + "-")) {
+                console.log("sip")
+                menuSeccion += `<a href="` + urlPagina + `" class="menu-seccion-link">` + nombres[index] + `</a>`;
+            }
+            index += 1;
+        }
+    }
+    document.getElementById("menu-de-seccion").innerHTML = menuSeccion;
 }
