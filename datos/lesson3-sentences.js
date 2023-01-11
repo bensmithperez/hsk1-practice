6c1f0d615ca6bac6e29b9ad80d5c62584aba4f34
bensmithperez <bsmith@scnctech.com>
2023-01-10 19:59:04 -0800

    agrego vocabulario a lesson3

--- datos/lesson3-sentences.js
+++ datos/lesson3-sentences.js
@@ -1,5 +1,8 @@
-let lesson3_sentences_indiceMax = 2;
+let lesson3_sentences_indiceMax = 5;
 let lesson3_sentences_listaCompletaDeDatos = '[' +
     '{"id":1,"word":"你在哪里？","pinyin":"nǐ zài nǎlǐ","meaning":"where are you?"},' +
-    '{"id":2,"word":"我在美国。","pinyin":"wǒ zài měiguó","meaning":"I\'m in the USA."}' +
+    '{"id":2,"word":"我在美国。","pinyin":"wǒ zài měiguó","meaning":"I\'m in the USA."},' +
+    '{"id":3,"word":"现在你在哪里？","pinyin":"xiànzài nǐ zài nǎlǐ?","meaning":"Where are you now?"},' +
+    '{"id":4,"word":"现在我在家。","pinyin":"xiànzài wǒ zàijiā.","meaning":"I am at home now"},' +
+    '{"id":5,"word":"你去哪里？","pinyin":"nǐ qù nǎlǐ","meaning":"Where are you going now?"}' +
     ']';
--- datos/lesson3-vocab.js
+++ datos/lesson3-vocab.js
@@ -1,4 +1,4 @@
-let lesson3_vocab_indiceMax = 9;
+let lesson3_vocab_indiceMax = 14;
 let lesson3_vocab_listaCompletaDeDatos = '[' +
     '{"id":1,"word":"家","pinyin":"jiā","meaning":"home, house"},' +
     '{"id":2,"word":"学校","pinyin":"xuéxiào","meaning":"school"},' +
@@ -8,5 +8,10 @@ let lesson3_vocab_listaCompletaDeDatos = '[' +
     '{"id":6,"word":"商场","pinyin":"shāngchǎng","meaning":"shopping mall, department store"},' +
     '{"id":7,"word":"飞机场","pinyin":"fēijī chǎng","meaning":"airport"},' +
     '{"id":8,"word":"饭店","pinyin":"fàndiàn","meaning":"restaurant"},' +
-    '{"id":9,"word":"咖啡店","pinyin":"kāfēi diàn","meaning":"coffee shop"}' +
+    '{"id":9,"word":"咖啡店","pinyin":"kāfēi diàn","meaning":"coffee shop"},' +  
+    '{"id":10,"word":"昨天","pinyin":"zuótiān","meaning":"yesterday"},' +
+    '{"id":11,"word":"现在","pinyin":"xiànzài","meaning":"right now"},' +
+    '{"id":12,"word":"今天","pinyin":"jīntiān","meaning":"today"},' +
+    '{"id":13,"word":"明天","pinyin":"míngtiān","meaning":"tomorrow"},' +
+    '{"id":14,"word":"去","pinyin":"qù","meaning":"to go somewhere"}' +
     ']';
