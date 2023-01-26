e60580c7867db0afd89d7eac6edcd31efe31a063
bensmithperez <bsmith@scnctech.com>
2023-01-25 17:51:25 -0800

    correcciones en lesson 5

--- datos/lesson5-sentences.js
+++ datos/lesson5-sentences.js
@@ -9,10 +9,10 @@ let listaCompletaDeDatos = [
     { "id": 8, "word": "你有中国的钱吗？", "pinyin": "Nǐ yǒu zhòng guó de qián ma?", "meaning": "do you have any chinese money?" },
     { "id": 9, "word": "你是谁？", "pinyin": "Nǐ shì shéi?", "meaning": "who are you?" },
     { "id": 10, "word": "谁是老师。", "pinyin": "Shéi shì lǎoshī.", "meaning": "who is the teacher?" },
-    { "id": 11, "word": "他是谁？", "pinyin": "Tā shì shéi?", "meaning": "who is she?" },
+    { "id": 11, "word": "他是谁？", "pinyin": "Tā shì shéi?", "meaning": "who is he?" },
     { "id": 12, "word": "谁是你的妈妈？", "pinyin": "Shéi shì nǐ de māmā?", "meaning": "who is your mother?" },
     { "id": 13, "word": "这是谁的男朋友？", "pinyin": "Zhè shì shéi de nán péngyǒu?", "meaning": "whose boyfriend is this?" },
-    { "id": 14, "word": "谁在书店", "pinyin": "Shéi zài shūdiàn", "meaning": "who's at the bookstore?" },
-    { "id": 15, "word": "谁有车", "pinyin": "shéi yǒu chē", "meaning": "Who has a car?" },
+    { "id": 14, "word": "谁在书店？", "pinyin": "Shéi zài shūdiàn", "meaning": "who's at the bookstore?" },
+    { "id": 15, "word": "谁有车？", "pinyin": "shéi yǒu chē", "meaning": "Who has a car?" },
     { "id": 16, "word": "昨天谁去了公司？", "pinyin": "zuótiān shéi qùle gōngsī?", "meaning": "who went to the office yesterday?" }
 ];
