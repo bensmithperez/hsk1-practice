5909d4bc5da46d42bc2dfdd9569ae10915fb16b1
bensmithperez <bsmith@scnctech.com>
2023-02-22 19:48:24 -0800

    agrego a lesson 6

--- datos/introduction-vocab.js
+++ datos/introduction-vocab.js
@@ -1,6 +1,6 @@
 // let numeros_indiceMax = 12;
 let listaCompletaDeDatos = [
-    { "id": 1, "word": "零｜〇", "pinyin": "líng ｜ líng", "meaning": "zero" },
+    { "id": 1, "word": "零", "pinyin": "líng ｜ líng", "meaning": "zero" },
     { "id": 2, "word": "一", "pinyin": "yī", "meaning": "one" },
     { "id": 3, "word": "二", "pinyin": "èr", "meaning": "two" },
     { "id": 4, "word": "三", "pinyin": "sān", "meaning": "three" },
@@ -11,5 +11,5 @@ let listaCompletaDeDatos = [
     { "id": 9, "word": "八", "pinyin": "bā", "meaning": "eight" },
     { "id": 10, "word": "九", "pinyin": "jiǔ", "meaning": "nine" },
     { "id": 11, "word": "十", "pinyin": "shí", "meaning": "ten" },
-    { "id": 12, "word": "一百", "pinyin": "bǎi", "meaning": "one hundred" }
+    { "id": 12, "word": "一百", "pinyin": "yī bǎi", "meaning": "one hundred" }
 ];
